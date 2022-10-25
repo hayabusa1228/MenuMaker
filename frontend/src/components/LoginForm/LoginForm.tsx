9b853e2c83b4f155e0cee1c2b62aa190c8ff7235
hayabusa <tsujinishihayato1228@outlook.jp>
2022-10-25 20:10:59 +0900

    login frontend2

--- frontend/src/components/LoginForm/LoginForm.tsx
+++ frontend/src/components/LoginForm/LoginForm.tsx
@@ -4,10 +4,11 @@ import Link  from "next/link";
 const LoginForm: React.FC = () => {
   return (
      <div className="container mx-auto md:w-3/5 w-4/5">
-      <div className="flex flex-col justify-center items-center my-20 bg-sky-100 rounded-xl">
+      {/* <div className="flex flex-col justify-center items-center my-20 bg-sky-100 rounded-xl"> */}
+      <div className="flex flex-col justify-center items-center my-20 border-lime-500 border-2 border-dashed rounded-xl">
         <div className="font-mono mt-10 text-center font-bold text-5xl italic text-lime-600 ">Menu Maker</div>
-        <input type="text" placeholder="ユーザー名" className=" rounded bg-white-100 h-8 mt-10 mx-5"/>
-        <input type="text" placeholder="パスワード" className="rounded bg-white-100  h-8 mt-5 mx-5"/>
+        <input type="text" placeholder="ユーザー名" className=" rounded bg-gray-50 h-8 mt-10 mx-5 border-gray-200 border-2"/>
+        <input type="text" placeholder="パスワード" className="rounded bg-gray-50  h-8 mt-5 mx-5 border-gray-200 border-2" />
         <button className="bg-blue-500 rounded-md font-mono font-medium text-white text-xl h-10 my-5 mx-20 hover:bg-blue-400">ログイン</button>
         <Link href="./signUp">
           <span className="underline mb-5">
--- frontend/src/components/SignUpForm/SignUpForm.tsx
+++ frontend/src/components/SignUpForm/SignUpForm.tsx
@@ -1,11 +1,11 @@
 const SignUpForm: React.FC =  () => {
   return (
     <div className="container mx-auto md:w-3/5 w-4/5">
-      <div className="flex flex-col justify-center items-center my-20 bg-sky-100 rounded-xl">
+      <div className="flex flex-col justify-center items-center my-20  border-lime-500 border-2 border-dashed rounded-xl">
         <div className="font-mono mt-10 text-center font-bold text-5xl italic text-lime-600 ">Menu Maker</div>
         <div className="text-xl mt-5">アカウント登録</div>
-        <input type="text" placeholder="ユーザー名" className=" rounded bg-white-100 h-8 mt-10 mx-5"/>
-        <input type="text" placeholder="パスワード" className="rounded bg-white-100  h-8 mt-5 mx-5"/>
+        <input type="text" placeholder="ユーザー名" className=" rounded bg-gray-50 h-8 mt-10 mx-5 border-gray-200 border-2"/>
+        <input type="text" placeholder="パスワード" className="rounded bg-gray-50  h-8 mt-5 mx-5 border-gray-200 border-2" />
         <button className="bg-blue-500 rounded-md font-mono font-medium text-white text-xl h-10 my-5 mx-20 hover:bg-blue-400 p-2">登録</button>
       </div>
       </div>
