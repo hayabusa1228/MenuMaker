8567d53c95b475153f12fd4a1532e73e4acb62ae
hayabusa <tsujinishihayato1228@outlook.jp>
2022-10-29 14:19:41 +0900

    all complete login func

--- backend/controllers/LoginResisterHandler.go
+++ backend/controllers/LoginResisterHandler.go
@@ -2,23 +2,27 @@
 package controller
 
 import (
+	"log"
 	_ "log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 
 	"github.com/hayabusa1228/MenuCreator/database"
 	"github.com/hayabusa1228/MenuCreator/structs"
 )
 
 const db_file_path = "./database/userinfo.db"
+var hash_add_string = "bvslfjvbsbvslnblsnblwabnerbreblbnefbn.febnfbf.bneksfbnfbnefbnfbnfk;b;fkb;kfnb;febfektb"
+
 
 
 func LoginHandler(c *gin.Context){
 	var LoginInfo structs.UserInfo
 
 	if err := c.ShouldBindJSON(&LoginInfo); err == nil {
-		// log.Println("Check login info")
+		LoginInfo.PassWord +=  hash_add_string
 		if(database.CheckUserNameAndPassword(&LoginInfo, db_file_path)){
 			c.JSON(http.StatusOK, gin.H{
 				"status" : "OK",
@@ -39,6 +43,18 @@ func ResgisterHandler(c *gin.Context){
 
 	if err := c.ShouldBindJSON(&RegisterInfo); err == nil {
 		// RegisterInfo.ID = config.LoadAndChangeIDJsonFile()
+		hashed_password, err := bcrypt.GenerateFromPassword([]byte(RegisterInfo.PassWord + hash_add_string), 10)
+		if err != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"status" : "OK",
+			})
+			return
+		}
+
+		RegisterInfo.PassWord = string(hashed_password)
+
+		// log.Println("password",bcrypt.CompareHashAndPassword([]byte(RegisterInfo.PassWord), []byte("1228"+ hash_add_string)))
+		log.Println(RegisterInfo.PassWord)
 		result := database.RegisterUserToDB(&RegisterInfo, db_file_path)
 		if (result){
 			// log.Println("Send register response from server to client")
--- backend/database/db.go
+++ backend/database/db.go
@@ -1,11 +1,14 @@
 package database
 
 import (
+	_"log"
 
 	"github.com/hayabusa1228/MenuCreator/structs"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
+
 
 func CreateUserInfoDB(filepath string){
 	db, err := gorm.Open(sqlite.Open(filepath), &gorm.Config{})
@@ -20,10 +23,15 @@ func CheckUserNameAndPassword(logininfo *structs.UserInfo, filepath string) bool
   if err != nil {
     return false
   }
+	var databaseinfo structs.UserInfo
   // 入力された情報が適切であるか
-	if err := db.First(&logininfo,"username = ? AND password = ?", logininfo.UserName, logininfo.PassWord); err.Error == nil {
-		return true
+	if err := db.First(&databaseinfo,"username = ?", logininfo.UserName); err.Error == nil {
+		// passwordが正しいかどうか
+		if err := bcrypt.CompareHashAndPassword([]byte(databaseinfo.PassWord), []byte(logininfo.PassWord)); err == nil {
+			return true
+		}
 	}
+	// log.Println("Can't login")
 
 	return false
 }
@@ -39,6 +47,7 @@ func checkSameNameUser(db *gorm.DB, registerinfo *structs.UserInfo) bool{
 
 func RegisterUserToDB(registerinfo *structs.UserInfo, filepath string) bool{
 	db, err := gorm.Open(sqlite.Open(filepath), &gorm.Config{})
+
   if err != nil {
     return false
   }
